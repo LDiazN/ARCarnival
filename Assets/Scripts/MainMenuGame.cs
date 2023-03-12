9dccac7c2063b031bd920ec4d94e472f7d7d9c4b
Luis Diaz <ldiazn98@gmail.com>
2023-03-11 20:41:09 -0400

    added original ARCarnival project I made long ago to this repo

---
+++ Assets/Scripts/MainMenuGame.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Vuforia;
+using UnityEngine.SceneManagement;
+
+public class MainMenuGame : MonoBehaviour
+{
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void PlayGame()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void QuitGame()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    public void StopGame()
+    {
+        SceneManager.LoadScene(2);
+    }
+}
